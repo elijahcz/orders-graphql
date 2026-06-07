ed7cf5a45c0390008cefeee17bf0766c4153fb72
Elias <elias.czh@gmail.com>
2026-06-07 17:45:14 -0600

    Server and Mongoose Setup For Mongo

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+const http = require('http');
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
-const PORT = 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server running at PORT ${PORT}`);
-});
+const { mongoConnect } = require('./src/services/mongo');
+const { start } = require('repl');
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+async function startServer() {
+    await mongoConnect();
+    
+    server.listen(PORT, () => {
+        console.log(`Server running at PORT ${PORT}`);
+    });
+}
+
+startServer();
