e9c66400ee6611a507dfd1e82b85a9da9bbc24d9
Elias <elias.czh@gmail.com>
2026-06-07 18:27:30 -0600

    260607 Created Models

---
+++ readme.txt
@@ -0,0 +1,2 @@
+Excercise source
+https://medium.com/@tokosbex/graphql-and-node-js-part-1-f54669498383
---
+++ src/models/user.mongoose.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 5,
+        maxlength: 225
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 5,
+        maxlength: 1024
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+const User = mongoose.model("User", userSchema);
